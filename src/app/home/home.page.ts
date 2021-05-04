c0983c6731b47fcece426e0dd8495269bcb91bd2
baypp <pphouone@gmail.com>
2021-05-04 13:14:09 +0200

    create home page

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -6,6 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  contacts = [
+    {name:'Directrice', phone:' 06.00.66.00.66'}
+    ,{name:'Assistante', phone:' 06.00.66.00.66'}
+    ,{name:'Designeur', phone:' 06.00.66.00.66'}
+
+  ]
 
   constructor() {}
 
